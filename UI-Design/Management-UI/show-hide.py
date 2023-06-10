4b699b63399aec7e2c284ff6b0fa238a94e8f4c7
Paul Sherwood <paulfsherwood@gmail.com>
2023-06-10 01:05:09 -0600

    almost done with first page for management

--- UI-Design/Management-UI/show-hide.py
+++ UI-Design/Management-UI/show-hide.py
@@ -1,9 +1,8 @@
 from PyQt6 import QtWidgets, uic, QtCore
+from PyQt6.QtCore import QTimer
 import qtawesome as qta
 import sys
 
-# Python QT Charts
-# https://www.youtube.com/watch?v=20ed0Ytkxuw&list=PLJ8t3BKaQLhPltjWNb0QApviqiXSqHeb6
 import os
 # import sys
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
@@ -11,26 +10,12 @@ from matplotlib.figure import Figure
 
 import numpy as np
 
-import random
+# import random
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         uic.loadUi('show-hide-test.ui', self)
-
-        # Set the icon for your QPushButton
-        # ri.pie-chart-line     # QT Charts
-        ## mdi.equalizer         # percent
-        ## ri.temp-hot-fill      # temperature
-        ## mdi.bullseye          # Nested Donut Chart
-        ## fa.code-fork          # Line Chart
-        ## fa.bar-chart-o        # Bar Chart
-        ## fa5.smile-wink        # Support
-        # mdi6.patreon          # Patreon
-        # mdi6.youtube-studio   # Youtube
-        # fa.paypal             # Paypal
-        # fa5.window-restore    # Maximize
-        # mdi.close-thick     # Close
 
         ############################################################################################################
         # ICON SETUP
@@ -48,6 +33,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.charts_pushButton.setIcon(qta.icon('mdi6.chart-areaspline', color='orange'))
 
         self.display_sample_bar_chart()
+        QTimer.singleShot(0, lambda: self.resizeEvent(None)) # force resize on start
+        self.set_awp()
 
         # set right_menu_widget as hidden when starting the application
         self.right_menu_widget.setHidden(True)
@@ -57,29 +44,21 @@ class MainWindow(QtWidgets.QMainWindow):
         # show and hide the right_menu_widget when menu_button is clicked
         self.right_menu_button.clicked.connect(lambda: self.right_menu_widget.setHidden(not self.right_menu_widget.isHidden()))
         self.left_menu_button.clicked.connect(lambda: self.left_menu_widget.setHidden(not self.left_menu_widget.isHidden()))
-        self.dashboard_pushButton.clicked.connect(self.show_dashboard)
-        self.cost_pushButton.clicked.connect(self.show_cost)
-        self.work_orders_pushButton.clicked.connect(self.show_work_orders_view)
-        self.inventory_pushButton.clicked.connect(self.show_inventory_view)
-        self.charts_pushButton.clicked.connect(self.show_charts_view)
+        self.dashboard_pushButton.clicked.connect(lambda: self.switch_page(self.dashboard_view, "DASHBOARD"))
+        self.cost_pushButton.clicked.connect(lambda: self.switch_page(self.cost_view, "COST"))
+        self.work_orders_pushButton.clicked.connect(lambda: self.switch_page(self.work_orders_view, "WORK ORDERS"))
+        self.inventory_pushButton.clicked.connect(lambda: self.switch_page(self.inventory_view, "INVENTORY"))
+        self.charts_pushButton.clicked.connect(lambda: self.switch_page(self.charts_view, "CHARTS"))
+
+    def switch_page(self, widget, title):
+        self.stackedWidget.setCurrentWidget(widget)
+        self.title_label.setText(title)
 
     def resizeEvent(self, event):
         # print("frame_16 size:", self.size())  # Print the size of frame_16
-        self.canvas.setGeometry(self.frame_16.rect())
-        event.accept()
-
-    ############################################################################################################
-    # SWAP STACKED WIDGETS (SCREENS)
-    def show_dashboard(self):
-        self.stacked_widget.setCurrentWidget(self.dashboard_view)
-    def show_cost(self):
-        self.stacked_widget.setCurrentWidget(self.cost_view)
-    def show_work_orders_view(self):
-        self.stacked_widget.setCurrentWidget(self.work_orders_view)
-    def show_inventory_view(self):
-        self.stacked_widget.setCurrentWidget(self.inventory_view)
-    def show_charts_view(self):
-        self.stacked_widget.setCurrentWidget(self.charts_view)
+        self.canvas.setGeometry(self.dashboard_frame_left.rect())
+        if event:
+            event.accept()
 
     def display_sample_bar_chart(self):
         # Generate sample data for the bar chart
@@ -103,29 +82,20 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # Create a FigureCanvas widget for the plot
         self.canvas = FigureCanvas(self.fig)
-        self.canvas.setParent(self.frame_16)
+        self.canvas.setParent(self.dashboard_frame_left)
         self.canvas.setGeometry(self.rect())
-
-        # # Create a QVBoxLayout for frame_16
-        # layout = QtWidgets.QVBoxLayout(self.frame_16)
-        # layout.addWidget(self.canvas)
-
-        # # Set the QVBoxLayout as the layout for frame_16
-        # self.frame_16.setLayout(layout)
-
-        # # Set a tight layout for the frame
-        # layout.setContentsMargins(0, 0, 0, 0)  # Set layout margins to zero
-        # layout.setSpacing(0)  # Set spacing between widgets to zero
-        # layout.addStretch(1)  # Add a stretchable space at the end of the layout
 
         # Show the canvas and redraw
         self.canvas.setVisible(True)
         self.canvas.draw()
 
-
-
-
-
+    def set_awp(self):
+        # set pri_1_count label
+        self.set_1_count.setText(str("HDD needs to be replaced"))
+        self.set_2_count.setText(str("Preflight"))
+        self.set_3_count.setText(str("Purchase request"))
+        self.set_4_count.setText(str("Awaiting Engineering"))
+        self.set_5_count.setText(str("..."))
 
 
 
