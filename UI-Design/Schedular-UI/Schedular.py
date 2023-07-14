cb3e3ec643ff7e0453cc923526f93e82dde73e05
PaulFSherwood <PaulFSherwood@gmail.com>
2023-07-13 18:17:03 -0600

    started schedule test

---
+++ UI-Design/Schedular-UI/Schedular.py
@@ -0,0 +1,15 @@
+from PyQt6.QtWidgets import QWidget, QApplication
+from PyQt6.uic import loadUi
+import sys
+
+class Window(QWidget):
+    def __init__(self):
+        super(Window, self).__init__()
+        loadUi("Schedular.ui", self)
+
+    
+if __name__ == "__main__":  
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
